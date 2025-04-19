f320cdf10469022ea373157e93acbea933b57890
mhasnain23 <tryfrelancer23@gmail.com>
2025-04-19 18:51:08 +0500

    Add music library and news API integration to Jarvis

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pyttsx3
 import speech_recognition as sr
 import webbrowser
-import os
-import wikipedia
+import musicLibrary
+import dotenv
+import requests
+
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
+newsapi = dotenv.dotenv_values("NEWS_API_KEY")
 
 
 def speak(text):
@@ -13,19 +16,54 @@ def speak(text):
     engine.runAndWait()
 
 
+def processCommand(c):
+    # open websites
+    if "open" in c.lower():
+        url = "https://" + c.lower().replace("open ", "") + ".com"
+        speak("Opening " + c.lower().replace("open ", ""))
+        webbrowser.open(url)
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        speak("Playing " + song)
+        # Check if the song is in the music library
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+    elif "news" in c.lower():
+        r = requests.get(
+            f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}"
+        )
+        if r.status_code == 200:
+            data = r.json()
+
+            articles = data.get("articles", [])
+            
+            for article in articles:
+                speak(article["title"])
+
+
 if __name__ == "__main__":
     speak("Initializing Jarvis...")
 
     while True:
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening..")
-            audio = r.listen(source)
 
         # Recognize the speech using Google Speech Recognition
+        print("Recognizing...")
         try:
-            print("You said: " + r.recognize_google(audio))
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
+            with sr.Microphone() as source:
+                print("Listening..")
+                audio = r.listen(source, timeout=2)
+            word = r.recognize_google(audio)
+            if word.lower() == "jarvis":
+                speak("Yes, how can I help you?")
+
+                # listen for the command
+            with sr.Microphone() as source:
+                print("Jarvis Active..")
+                audio = r.listen(source)
+                command = r.recognize_google(audio)
+
+                processCommand(command)
+
+        except Exception as e:
             print("Error; {0}".format(e))
---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "skyfall": "https://youtu.be/sZrTJesvJeo?list=RDMM",
+    "slave": "https://youtu.be/0v1a2g3h4i5?list=RDMM",
+    "soap": "https://youtu.be/AvqMlj1Nh8E?list=RDMM",
+    "funk": "https://www.youtube.com/watch?v=FVAS9ETK1BM&list=RDMM&index=24&pp=8AUB",
+    "jhol": "https://www.youtube.com/watch?v=BWczaSneA0Q&list=RDMM&index=39&pp=8AUB",
+}
